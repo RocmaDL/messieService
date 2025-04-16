2848f95b4a48db4e74f87a4e8697a6f4586b3e34
RocmaDL <misterpoint0305@gmail.com>
2025-04-16 18:32:36 +0200

    Ajout de la configuration de base pour une application Nuxt, y compris des fichiers de configuration TypeScript, des pages, un fichier robots.txt, et une structure de projet initiale.

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav>
+      <h1>CacaLand</h1>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/caca">Caca</NuxtLink></li>
+      </ul>
+    </nav>  
+  </header>
+
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};  
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the main content of the home page.</p>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+/* empty style block */
+</style>
