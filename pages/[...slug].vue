cafe3be6a9600a352d750150220228be5973bb72
RocmaDL <misterpoint0305@gmail.com>
2025-04-25 15:05:14 +0200

    Ajout d'une page de redirection pour les routes non existantes vers la page d'accueil.

---
+++ pages/[...slug].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Cette page agit seulement comme un attrape-tout pour rediriger les routes non existantes -->
+  </div>
+</template>
+
+<script setup>
+// Ce fichier capture toutes les routes qui ne correspondent à aucune autre page
+// et les redirige vers la page d'accueil
+definePageMeta({
+  middleware: [
+    function (to) {
+      return navigateTo("/");
+    },
+  ],
+});
+</script>
